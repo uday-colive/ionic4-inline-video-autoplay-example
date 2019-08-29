a9b5f71bd75f6bedc749c05181d2bbe1bfa44368
Mateus Duraes <mduraes1994@gmail.com>
2019-08-29 09:28:02 +0200

    Youtube and vimeo with autoplay inline

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare var YT;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   constructor() {}
 
+  ngOnInit() {
+    setTimeout(() => {
+      let player;
+      console.log('yt', YT),
+        (player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          playerVars: {
+            autoplay: 1,
+            controls: 0,
+            playsinline: 1
+          },
+          events: {
+            onReady: event => event.target.playVideo(),
+            onStateChange: state => console.log('state', state)
+          }
+        }));
+    }, 10_000);
+  }
 }
